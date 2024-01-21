ffdd647cf66f975e5468fc0528200f6b33a378f9
xiyu1984 <shanellyy@126.com>
2024-01-22 00:27:12 +0800

    clean

--- circuit/tcircuit.go
+++ circuit/tcircuit.go
@@ -1,8 +1,6 @@
 package circuit
 
 import (
-	"errors"
-
 	"github.com/consensys/gnark/frontend"
 )
 
@@ -15,17 +13,13 @@ type TCircuit struct {
 // Define declares the circuit logic. The compiler then produces a list of constraints
 // which must be satisfied (valid witness) in order to create a valid zk-SNARK
 func (circuit *TCircuit) Define(api frontend.API) error {
-	if len(circuit.X) > 0 {
-		res := circuit.X[0]
-		for i := 1; i < len(circuit.X); i++ {
-			res = api.Mul(res, circuit.X[i])
-		}
-
-		res = api.Add(res, circuit.S)
-
-		api.AssertIsEqual(circuit.Y, res)
-		return nil
-	} else {
-		return errors.New("invalid length")
+	res := circuit.X[0]
+	for i := 1; i < len(circuit.X); i++ {
+		res = api.Mul(res, circuit.X[i])
 	}
+
+	res = api.Add(res, circuit.S)
+
+	api.AssertIsEqual(circuit.Y, res)
+	return nil
 }
